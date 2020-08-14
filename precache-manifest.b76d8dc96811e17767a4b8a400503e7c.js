7ca558c39298b11207c22df7c9d64060c4d0538f
ali <aliabbasi806@gmail.com>
2020-08-14 20:57:06 +0430

    Updates

---
+++ precache-manifest.b76d8dc96811e17767a4b8a400503e7c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "064ac00b3811c7a583ebb663180c1097",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "7d8f0458e05727635809",
+    "url": "/resume/static/css/2.72ebba26.chunk.css"
+  },
+  {
+    "revision": "226179564865dac92052",
+    "url": "/resume/static/css/main.8732f51f.chunk.css"
+  },
+  {
+    "revision": "7d8f0458e05727635809",
+    "url": "/resume/static/js/2.fc2fd104.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/resume/static/js/2.fc2fd104.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "226179564865dac92052",
+    "url": "/resume/static/js/main.d386953e.chunk.js"
+  },
+  {
+    "revision": "8c1d914a1677cea53979",
+    "url": "/resume/static/js/runtime-main.75d8c91e.js"
+  },
+  {
+    "revision": "2a32e84a3f2e6555e0f936e20785c5d2",
+    "url": "/resume/static/media/Cpp.2a32e84a.webp"
+  },
+  {
+    "revision": "7c6983609537d20f7a152ea73a476215",
+    "url": "/resume/static/media/Javascript.7c698360.webp"
+  },
+  {
+    "revision": "e318f8fe18cdde2d12b713dfb8e6fc32",
+    "url": "/resume/static/media/Python.e318f8fe.webp"
+  },
+  {
+    "revision": "e1bc4e47307e572ba4a4eeb4fa66d6c2",
+    "url": "/resume/static/media/navigate.e1bc4e47.png"
+  }
+]);
